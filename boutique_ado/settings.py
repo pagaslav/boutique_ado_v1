72e1c60155f3f8ae87105bcc91a1bca410bbfa2c
Artem Bryzh <aab@AMBM2.local>
2024-09-10 15:09:51 +0100

    Start deployment to Heroku

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -26,7 +27,7 @@ SECRET_KEY = 'p@ei#69*b*zz3u4yie-$()@cy^l(+x9&@6ypx+r0lm(3%_9hr7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://boutiq-ado-ci-8d4ffe06054d.herokuapp.com/', 'localhost']
 
 # Application definition
 
@@ -119,13 +120,17 @@ WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -173,6 +178,8 @@ FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 # Stripe
 FREE_DELIVERY_THRESHOLD = 50
--- checkout/signals.py
+++ checkout/signals.py
@@ -1,25 +1,39 @@
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from django.contrib.auth.models import User
-from profiles.models import UserProfile  # предполагается, что модель профиля находится в profiles/models.py
+from profiles.models import UserProfile
 from .models import OrderLineItem
+from django.db.utils import IntegrityError
 
-# Signal to create a user profile when a new user is created
+# Signal to create or update a user profile when a new user is created
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
+    """
+    Create a user profile if one doesn't exist and skip for superusers.
+    """
+    if instance.is_superuser:  # Skip superuser profile creation
+        return
+
     if created:
-        UserProfile.objects.create(user=instance)
+        try:
+            UserProfile.objects.create(user=instance)
+        except IntegrityError:
+            pass  # Handle IntegrityError when profile already exists
 
 # Signal to save the user profile when user data is updated
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
-    instance.userprofile.save()
+    """
+    Save the user profile, if it exists.
+    """
+    if hasattr(instance, 'userprofile'):
+        instance.userprofile.save()
 
 # Signal to update the order total when an order line item is created or updated
 @receiver(post_save, sender=OrderLineItem)
 def update_on_save(sender, instance, created, **kwargs):
     """
-    Update the order total when a line item is created or updated
+    Update the order total when a line item is created or updated.
     """
     instance.order.update_total()
 
@@ -27,6 +41,6 @@ def update_on_save(sender, instance, created, **kwargs):
 @receiver(post_delete, sender=OrderLineItem)
 def update_on_delete(sender, instance, **kwargs):
     """
-    Update the order total when a line item is deleted
+    Update the order total when a line item is deleted.
     """
     instance.order.update_total()
