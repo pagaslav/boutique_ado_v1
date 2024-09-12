c153febce0ed2836d16133815b6db30fd0e468b3
Artem Bryzh <aab@AMBM2.local>
2024-09-12 17:47:48 +0100

    Update allauth settings

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -1,3 +1,4 @@
+
 """
 Django settings for boutique_ado project.
 
@@ -26,8 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['ckz8780-boutique-ado.herokuapp.com', 'localhost']
-
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'boutiq-ado-ci-8d4ffe06054d.herokuapp.com']
 
 # Application definition
 
@@ -94,6 +94,7 @@ TEMPLATES = [
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
+
 AUTHENTICATION_BACKENDS = (
     # Needed to login by username in Django admin, regardless of `allauth`
     'django.contrib.auth.backends.ModelBackend',
@@ -131,7 +132,6 @@ else:
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -203,6 +203,8 @@ FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 # Stripe
 FREE_DELIVERY_THRESHOLD = 50
