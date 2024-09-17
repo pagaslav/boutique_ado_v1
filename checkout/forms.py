93db089813c40923555bc10f6cf4591b7b5a28c4
Artem Bryzh <aab@AMBM2.local>
2024-09-17 15:43:10 +0100

    Updated OrderForm

--- checkout/forms.py
+++ checkout/forms.py
@@ -1,24 +1,21 @@
 from django import forms
 from django_countries.fields import CountryField
-from .models import Order
 from django_countries.widgets import CountrySelectWidget
-
+from .models import Order
 
 class OrderForm(forms.ModelForm):
-    country = CountryField(blank_label='Select Country').formfield(widget=CountrySelectWidget())
+    country = CountryField(blank_label='(Select country)').formfield(widget=CountrySelectWidget())
 
     class Meta:
         model = Order
-        fields = ('full_name', 'email', 'phone_number',
-                  'street_address1', 'street_address2',
-                  'town_or_city', 'postcode', 'country',
-                  'county',)
+        fields = (
+            'full_name', 'email', 'phone_number',
+            'street_address1', 'street_address2',
+            'town_or_city', 'postcode', 'country',
+            'county',
+        )
 
     def __init__(self, *args, **kwargs):
-        """
-        Add placeholders and classes, remove auto-generated
-        labels and set autofocus on first field
-        """
         super().__init__(*args, **kwargs)
 
         placeholders = {
@@ -35,10 +32,7 @@ class OrderForm(forms.ModelForm):
         self.fields['full_name'].widget.attrs['autofocus'] = True
         for field in self.fields:
             if field != 'country':
-                if self.fields[field].required:
-                    placeholder = f'{placeholders[field]} *'
-                else:
-                    placeholder = placeholders[field]
+                placeholder = placeholders.get(field, '')
                 self.fields[field].widget.attrs['placeholder'] = placeholder
             self.fields[field].widget.attrs['class'] = 'stripe-style-input'
             self.fields[field].label = False
